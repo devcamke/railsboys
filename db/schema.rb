ab86a284c3498aca52cccca447ac4844681c3e04
Dennis Kamau <devjam.ke@gmail.com>
2024-01-16 17:16:23 +0300

    Create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_16_133245) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_141558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.bigint "idea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_16_133245) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "ideas"
 end
