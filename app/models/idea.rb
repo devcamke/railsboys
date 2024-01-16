c689c20698aceb2ca0dd2ba549994ed775078a29
Dennis Kamau <devjam.ke@gmail.com>
2024-01-16 16:52:22 +0300

    Attaching the picture uploader to the idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,8 @@
 class Idea < ApplicationRecord
+
+mount_uploader :picture, PictureUploader
+
+validates :name, presence: true
+validates :description, presence: true
+validates :picture, presence: true
 end
