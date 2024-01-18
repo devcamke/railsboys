5b7293632a58b6ed0d916442ec6accc455f4b9fe
Dennis Kamau <devjam.ke@gmail.com>
2024-01-18 21:43:09 +0300

    Add and write tests using Rspec

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -5,8 +5,20 @@ RSpec.describe Idea do
     idea = Idea.create!(name: "My Awesome Idea Name") # creating a new idea 'instance'
     expect(idea.name).to eq("My Awesome Idea Name") # this is our expectation
   end
+  
   it "has a description" do
     idea = Idea.create!(description: "My Awesome Idea Description")
     expect(idea.description).to eq("My Awesome Idea Description")
+  end
+
+  it "has comments" do
+    idea = Idea.create!(name: "My Awesome Idea Name")
+    comment = Comment.create!(
+      user_name: "My name",
+      body: "My helpful comment",
+      idea: idea # Link the comment to the idea
+    )
+
+    expect(comment.idea).to eq(idea)
   end
 end
